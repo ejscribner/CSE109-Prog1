533f1dc91ff980858d2b3a059fbeee1d402abe36
ejscribner <ejscribner@gmail.com>
2018-09-03 11:52:48 -0400

    fixes

--- prog1.c
+++ prog1.c
@@ -8,19 +8,28 @@ Program #1
 
 #include <stdio.h>
 #include <string.h>
-
+#include <stdlib.h>
 int main(int argc, char** argv) {
+    char* word = NULL;
+    word = (char*)malloc(5 * sizeof(char));
+    strcpy(word, "bird");
+    word[0] = 'b';
+    word[1] = 'i';
+    word[2] = 'r';
+    word[3] = 'd';
+    word[4] = '\0';
+
     for(int i = 0; i < argc; i++) {
-        printf(argv[i]);
+        fprintf(stdout, "Arg %d: %s\n", i, argv[i]);
         printf("\n");
     }
-
-    printf("Enter a value: ");
-    char str[100];
-    int i;
-    scanf("%s %d", str, &i);
-    printf("\nYou entered: ");
-    printf(str);
-    printf("\n");
+    free(word);
+    word = NULL;
+    fprintf(stdout, "Word is %s\n", word);
+    printf("Enter a value: \n");
+    fscanf(stdin, "%m[^\n]s", &word);
+    fprintf(stdout, "\nYou entered: %s\n", word);
+    free(word);
+    word = NULL;
     return 0;
 }
