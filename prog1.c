7932dd1556e36a50f65abb297e032c04b72bafa3
Elliot Scribner <ejs320@callisto.cse.lehigh.edu>
2018-09-03 12:03:28 -0400

    cleanup

--- prog1.c
+++ prog1.c
@@ -9,27 +9,14 @@ Program #1
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-int main(int argc, char** argv) {
-    char* word = NULL;
-    word = (char*)malloc(5 * sizeof(char));
-    strcpy(word, "bird");
-    word[0] = 'b';
-    word[1] = 'i';
-    word[2] = 'r';
-    word[3] = 'd';
-    word[4] = '\0';
-
+int main(int argc, char** argv) { 
+    char* word;
     for(int i = 0; i < argc; i++) {
         fprintf(stdout, "Arg %d: %s\n", i, argv[i]);
         printf("\n");
     }
-    free(word);
-    word = NULL;
-    fprintf(stdout, "Word is %s\n", word);
-    printf("Enter a value: \n");
+    printf("Enter a value: ");
     fscanf(stdin, "%m[^\n]s", &word);
     fprintf(stdout, "\nYou entered: %s\n", word);
-    free(word);
-    word = NULL;
     return 0;
 }
