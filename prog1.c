5fcd9fd750ececfe107db26a43d53016f336f929
ejscribner <ejscribner@gmail.com>
2018-09-04 19:54:03 -0400

    reverse, toggle, functional args

--- prog1.c
+++ prog1.c
@@ -11,23 +11,66 @@ Program #1
 #include <stdlib.h>
 #include <readline/readline.h>
 #include <readline/history.h>
-
+#include <ctype.h>
 void reverseWord(char* line, size_t length);
 void reverseUtil(char *start, char* end);
+void toggle(char* line, size_t length);
+void rotate(char* line, size_t length);
+size_t removeDigits(char* line, size_t length);
 
 int main(int argc, char** argv) {
     char* line;
+    int isReverse = 0;
+    int isRotate = 0;
+    int isToggle = 0;
+    int isRemoveDigits = 0;
+
+
+    line = readline("Enter a Value: ");
     for(int i = 0; i < argc; i++) {
+        if(strcmp(argv[i], "-r") == 0) {
+            isReverse = 1;
+        }
+        if(strcmp(argv[i], "-o") == 0) {
+            isRotate = 1;
+        }
+        if(strcmp(argv[i], "-t") == 0) {
+            isToggle = 1;
+        }
+        if(strcmp(argv[i], "-n") == 0) {
+            isRemoveDigits = 1;
+        }
         fprintf(stdout, "Arg %d: %s\n", i, argv[i]);
         printf("\n");
     }
+    if(isReverse) {
+        reverseWord(line, sizeof(line));
+    }
+    if(isToggle) {
+        toggle(line, sizeof(line));
+    }
+    if(isRotate) {
 
-    line = readline("Enter a Value: ");
-    reverseWord(line, sizeof(line));
+    }
+    if(isRemoveDigits) {
+
+    }
     fprintf(stdout, "Line is: %s\n", line);
     free(line);
     line = NULL;
     return 0;
+}
+
+void toggle(char* line, size_t length) {
+    char *wordEnd = line;
+    while(*wordEnd) {
+        if(isupper(*wordEnd)) {
+            *wordEnd = tolower(*wordEnd);
+        }else if(islower(*wordEnd)) {
+            *wordEnd = toupper(*wordEnd);
+        }
+        wordEnd++;
+    }
 }
 
 void reverseWord(char* line, size_t length) {
@@ -51,4 +94,12 @@ void reverseUtil(char *start, char* end) {
         *start++ = *end;
         *end-- = temp;
     }
+}
+
+void rotate(char* line, size_t length) {
+
+}
+
+size_t removeDigits(char* line, size_t length) {
+    return 0;
 }
