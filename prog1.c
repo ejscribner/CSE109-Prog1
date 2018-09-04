d92d2b8fa436ec52b9d65cd06b39f858dc8f539a
Elliot Scribner <ejs320@pan.cse.lehigh.edu>
2018-09-04 16:43:01 -0400

    ful of errors

--- prog1.c
+++ prog1.c
@@ -12,26 +12,23 @@ Program #1
 #include <readline/readline.h>
 #include <readline/history.h>
 
-//void reverse(char* input);
+void reverse(char input);
 int test();
 int main(int argc, char** argv) {
     int t = test();
-    fprintf(stdout, "%i", t);
-    //char* input;
+    char input[100];
     for(int i = 0; i < argc; i++) {
         fprintf(stdout, "Arg %d: %s\n", i, argv[i]);
         printf("\n");
     }
+    fprintf(stdout, "%i", t);
 
-//    fprintf(stdout, "Enter a value: ");
-//    fscanf(stdin, "%m[^\n]s", &input);
-   // input = "Test Testing";
-    printf("%s\n", readline("Enter a Value: "));
-   // fprintf(stdout, "\nYou entered: %s\n", input);
+    input = readline("Enter a Value: ");
+    fprintf(stdout, "\nYou entered: %s\n", input);
 
-   // free(input);
-
-   // reverse(input);
+    free(input);
+    input = NULL;
+    reverse(input);
 
     return 0;
 }
@@ -41,10 +38,10 @@ int test() {
     return 4;
 }
 
-//void reverse(char* input) {
-//    for(int i = 0; i < sizeof(input); i++) {
-//        printf("hi");
-//        printf((char*)input[i]);
-//        printf("\n");
-//    }
-//}
+void reverse(char input) {
+    for(int i = 0; i < sizeof(input); i++) {
+        printf("hi");
+        printf(input[i]);
+        printf("\n");
+    }
+}
